7d3758702c30678742f644350a66443a41b323c5
Stuff 1 <stuff1@chaanine.com>
2013-11-12 21:44:34 -0800

    done with layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "foo_bars/bax"
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  root 'static_pages#home'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
